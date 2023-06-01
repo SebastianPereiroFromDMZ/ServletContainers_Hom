13cd42be47f97202e03071b13ff218cbdb08db34
SebastianPereiro <kuzmin_konstantin@bk.ru>
2023-06-01 23:25:36 +0300

    add xml config

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,5 +1,7 @@
 package ru.netology.servlet;
 
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import ru.netology.controller.PostController;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
@@ -19,9 +21,10 @@ public class MainServlet extends HttpServlet {
 
   @Override
   public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
+    final var factory = new DefaultListableBeanFactory();
+    final var reader = new XmlBeanDefinitionReader(factory);
+    reader.loadBeanDefinitions("beans.xml");
+    controller = (PostController) factory.getBean("postController");
   }
 
   @Override
